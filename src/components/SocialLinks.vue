68c540ead6ab9e3625134fd78761567093733920
Quisper <507678128@qq.com>
2025-07-20 14:59:22 +0800

    修改错误

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script setup>
-import socialLinks from "@/home/assets/socialLinks.json";
+import socialLinks from "@/assets/socialLinks.json";
 
 // 社交链接提示
 const socialTip = ref("通过这里联系我吧");
